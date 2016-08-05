bdd61a6cd71f80cd41c8928681bd83826ae1e4aa
Mike Butrimov <mike.butrimov@gmail.com>
2016-08-05 20:30:07 +0300

    hmmm

--- app/src/main/java/org/unhack/bip38decrypt/DecodeActivity.java
+++ app/src/main/java/org/unhack/bip38decrypt/DecodeActivity.java
@@ -47,9 +47,7 @@ public class DecodeActivity extends AppCompatActivity {
                 decodePagerAdapter.CoolNavigateToTab(1,TABNUMBER,DecodeActivity.decodeSwipeHandler,false);
             }
         };
-
-
-
+        
         List<mFragment> fragments = new ArrayList<mFragment>();
         dInputFragment inputFragment = new dInputFragment();
         fragments.add(inputFragment);
@@ -68,8 +66,6 @@ public class DecodeActivity extends AppCompatActivity {
             viewPager.setPagingEnabled(false);
         }
     }
-
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
