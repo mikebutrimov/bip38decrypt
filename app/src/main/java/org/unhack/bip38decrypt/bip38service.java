2f64cfd9705d2f7f3e8727044468e3c18d8ee4e1
Mike Butrimov <mike.butrimov@gmail.com>
2016-08-15 15:59:30 +0300

    move all expensive computations to thread

--- app/src/main/java/org/unhack/bip38decrypt/bip38service.java
+++ app/src/main/java/org/unhack/bip38decrypt/bip38service.java
@@ -26,50 +26,44 @@ public class bip38service extends IntentService {
         Log.d("Service P",password);
         final String password2 = intent.getStringExtra("password2");
         final boolean needReEncrypt = intent.getBooleanExtra("reencrypt",false);
-        SecureCharSequence result = null;
+
+
         try {
-            result = Bip38.decrypt(wallet,password);
-            res = result.toString();
-            DumpedPrivateKey dumpedPrivateKey = new DumpedPrivateKey(res);
-            ECKey ecKey = dumpedPrivateKey.getKey();
-            address = ecKey.toAddress();
-            Log.d("Service addr",address);
-        } catch (InterruptedException e) {
+
+            Thread worker = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        res = Bip38.decrypt(wallet,password).toString();
+                        DumpedPrivateKey dumpedPrivateKey = new DumpedPrivateKey(res);
+                        ECKey ecKey = dumpedPrivateKey.getKey();
+                        address = ecKey.toAddress();
+                        Log.d("Service addr",address);
+                        if (needReEncrypt){
+                            res = Bip38.encryptNoEcMultiply(password2,res);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (AddressFormatException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (res != null) {
+                        Log.d("Service ", "Before Handler");
+                        Intent resIntent = new Intent(MainActivity.INTENT_FILTER);
+                        resIntent.putExtra("result",res);
+                        resIntent.putExtra("address",address);
+                        sendBroadcast(resIntent);
+                        Log.d("Service ", "After Handler");
+                    }
+                    IAM = false;
+                }
+            });
+            worker.setPriority(Thread.MAX_PRIORITY);
+            worker.start();
+
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (AddressFormatException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e){
-
         }
-        if (result != null) {
-            res = result.toString();
-            Log.d("Service res",res);
-        }
-        if (needReEncrypt){
-            try {
-                res = Bip38.encryptNoEcMultiply(password2,res);
-                Log.d("Service re res",res);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (AddressFormatException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        if (res != null) {
-            Log.d("Service ", "Before Handler");
-
-            Intent resIntent = new Intent(MainActivity.INTENT_FILTER);
-            resIntent.putExtra("result",res);
-            resIntent.putExtra("address",address);
-            sendBroadcast(resIntent);
-            Log.d("Service ", "After Handler");
-
-
-        }
-        else {
-        }
-        IAM = false;
     }
 }
